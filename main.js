faa0b0777c4058514a751889145f25de346c7aea
bernardow <bernardoritzel@gmail.com>
2023-02-28 07:53:14 -0300

    layout-ready-to-responsive

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        arrows: false,
+    });
+
+    $('.recomendations-carousel').slick({
+        autoplay: true,
+        centerMode: true,
+        slidesToShow: 3,
+        centerPadding: '24px',
+        arrows: false,
+    })
+
+})
